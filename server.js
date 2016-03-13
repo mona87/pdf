b2e054419b4f57c7c9aec42078498decf0c122d5
Ramona Bellamy <bellamy2727@yahoo.com>
2016-03-13 14:36:03 -0500

    server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 
 /* view at http://localhost:8080 */
+var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -12,7 +13,9 @@ app.get('/', function(req, res) {
 });
 
 
-
+app.listen(port, () => {
+    console.log('Service started on port :' + port);
+});
 
 
 // app.listen(8080);
